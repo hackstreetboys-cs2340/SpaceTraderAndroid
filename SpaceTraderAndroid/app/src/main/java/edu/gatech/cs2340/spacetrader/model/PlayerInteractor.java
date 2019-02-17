0ef9658a8c1f6260331d24cc4fe6466476afe6dd
Abby Stuart <abbystuart@lawn-128-61-39-178.lawn.gatech.edu>
2019-02-17 16:57:23 -0500

    player interactor methods

--- SpaceTraderAndroid/app/src/main/java/edu/gatech/cs2340/spacetrader/model/PlayerInteractor.java
+++ SpaceTraderAndroid/app/src/main/java/edu/gatech/cs2340/spacetrader/model/PlayerInteractor.java
@@ -1,8 +1,27 @@
 package edu.gatech.cs2340.spacetrader.model;
 
+import edu.gatech.cs2340.spacetrader.entity.Difficulty;
+import edu.gatech.cs2340.spacetrader.entity.Player;
+
 public class PlayerInteractor extends Interactor {
 
     public PlayerInteractor(Repository repo) {
         super(repo);
+    }
+
+    public void setSkillPoints(int pilotSkill, int engSkill, int tradeSkill, int fightSkill) {
+        Player player = getRepository().getMyPlayer();
+        player.setPilotSkill(pilotSkill);
+        player.setEngSkill(engSkill);
+        player.setTradeSkill(tradeSkill);
+        player.setFightSkill(fightSkill);
+    }
+
+    public void setName(String name) {
+        getRepository().getMyPlayer().setName(name);
+    }
+
+    public void setDifficulty(Difficulty diff) {
+        getRepository().getMyPlayer().setDifficulty(diff);
     }
 }
