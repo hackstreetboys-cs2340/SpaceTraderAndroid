393e56979ff46e459eba7d9edac74f9f6d4c81d6
Rahil Patel <rahilpatel99@gmail.com>
2019-02-19 19:59:33 -0500

    initial commit for iOS version

---
+++ SpaceTraderIOS/SpaceTraderIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceTraderIOS
+//
+//  Created by Rahil Patel on 2/19/19.
+//  Copyright © 2019 HackstreetBoys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
