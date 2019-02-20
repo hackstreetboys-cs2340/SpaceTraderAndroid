ef8a62a235df37e5d556f3f964885cd5853f85b6
Rahil Patel <rahilpatel99@gmail.com>
2019-02-20 03:36:45 -0500

    UI for menu and player config

---
+++ SpaceTraderIOS/SpaceTraderIOS/Colors.swift
@@ -0,0 +1,63 @@
+//
+//  Colors.swift
+//  SpaceTraderIOS
+//
+//  Created by Rahil Patel on 2/19/19.
+//  Copyright © 2019 HackstreetBoys. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors {
+    static let black = UIColor(r: 0, g: 0, b: 0, a: 1)
+    static let white = UIColor(r: 255, g: 255, b: 255, a: 1)
+    static let lightGray = UIColor(r: 122, g: 122, b: 122, a: 1)
+    
+    static let primaryColor = lightGray
+    static let secondaryColor = white
+    static let textColor = white
+    static let backgroundColor = black
+}
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
+    
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor {
+        return self.adjustBrightness(by: abs(percentage))
+    }
+    
+    /**
+     Create a darker color
+     */
+    func darker(by percentage: CGFloat = 30.0) -> UIColor {
+        return self.adjustBrightness(by: -abs(percentage))
+    }
+    
+    /**
+     Try to increase brightness or decrease saturation
+     */
+    func adjustBrightness(by percentage: CGFloat = 30.0) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        if self.getHue(&h, saturation: &s, brightness: &b, alpha: &a) {
+            if b < 1.0 {
+                /**
+                 Below is the new part, which makes the code work with black as well as colors
+                 */
+                let newB: CGFloat
+                if b == 0.0 {
+                    newB = max(min(b + percentage/100, 1.0), 0.0)
+                } else {
+                    newB = max(min(b + (percentage/100.0)*b, 1.0), 0,0)
+                }
+                return UIColor(hue: h, saturation: s, brightness: newB, alpha: a)
+            } else {
+                let newS: CGFloat = min(max(s - (percentage/100.0)*s, 0.0), 1.0)
+                return UIColor(hue: h, saturation: newS, brightness: b, alpha: a)
+            }
+        }
+        return self
+    }
+}
--- SpaceTraderIOS/SpaceTraderIOS/ViewController.swift
+++ SpaceTraderIOS/SpaceTraderIOS/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let startButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup afterloading the view, typically from a nib.
+        layoutUI()
+    }
+    
+    private func layoutUI() {
+        view.backgroundColor = Colors.backgroundColor
+        
+        let offset: CGFloat = 10
+        let height: CGFloat = 40
+        let size = CGSize(width: view.frame.width - (2 * offset), height: height)
+        let pos = CGPoint(x: offset, y: view.frame.height * 0.75)
+        startButton.frame = CGRect(origin: pos, size: size)
+        startButton.backgroundColor = Colors.primaryColor
+        startButton.setTitleColor(Colors.textColor, for: .normal)
+        startButton.layer.cornerRadius = 5
+        startButton.setTitle("Start", for: .normal)
+        startButton.addTarget(self, action: #selector(startTapped(_:)), for: .touchUpInside)
+        
+        view.addSubview(startButton)
     }
 
+    @objc private func startTapped(_ sender: Any) {
+        performSegue(withIdentifier: "mainToConfig", sender: sender)
+    }
 
 }
 
