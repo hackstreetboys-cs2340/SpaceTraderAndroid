9e6a0cb3723c07e370b9bf2c1491017bbe027cd1
Rahil Patel <rahilpatel99@gmail.com>
2019-02-21 19:14:49 -0500

    Added title

--- SpaceTraderIOS/SpaceTraderIOS/ViewController.swift
+++ SpaceTraderIOS/SpaceTraderIOS/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     let startButton = UIButton()
+    let titleLbl = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup afterloading the view, typically from a nib.
@@ -22,8 +24,8 @@ class ViewController: UIViewController {
         
         let offset: CGFloat = 10
         let height: CGFloat = 40
-        let size = CGSize(width: view.frame.width - (2 * offset), height: height)
-        let pos = CGPoint(x: offset, y: view.frame.height * 0.75)
+        var size = CGSize(width: view.frame.width - (2 * offset), height: height)
+        var pos = CGPoint(x: offset, y: view.frame.height * 0.75)
         startButton.frame = CGRect(origin: pos, size: size)
         startButton.backgroundColor = Colors.primaryColor
         startButton.setTitleColor(Colors.textColor, for: .normal)
@@ -31,7 +33,16 @@ class ViewController: UIViewController {
         startButton.setTitle("Start", for: .normal)
         startButton.addTarget(self, action: #selector(startTapped(_:)), for: .touchUpInside)
         
+        pos = CGPoint(x: offset, y: offset * 4)
+        size = CGSize(width: view.frame.width - 2 * offset, height: 80)
+        titleLbl.frame = CGRect(origin: pos, size: size)
+        titleLbl.text = "Space Trader"
+        titleLbl.textColor = Colors.textColor
+        titleLbl.font = UIFont(name: titleLbl.font.fontName, size: 40)
+        titleLbl.textAlignment = .center
+        
         view.addSubview(startButton)
+        view.addSubview(titleLbl)
     }
 
     @objc private func startTapped(_ sender: Any) {
