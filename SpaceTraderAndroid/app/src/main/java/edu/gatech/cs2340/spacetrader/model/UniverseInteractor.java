e1c42ae6fb1dce6cdca617845daf39291cfbb8a9
Caroline Devlin <carolinedevlin@lawn-128-61-60-116.lawn.gatech.edu>
2019-02-26 19:36:18 -0500

    Added Universe Interactor

---
+++ SpaceTraderAndroid/app/src/main/java/edu/gatech/cs2340/spacetrader/model/UniverseInteractor.java
@@ -0,0 +1,24 @@
+package edu.gatech.cs2340.spacetrader.model;
+
+import edu.gatech.cs2340.spacetrader.entity.Universe;
+
+public class UniverseInteractor extends Interactor {
+
+    /**
+     * constructor for universe interactor
+     *
+     * @param repo respository
+     */
+    public UniverseInteractor(Repository repo) {
+        super(repo);
+    }
+
+    /**
+     * setter for universe to interact (accesses Repository's setter)
+     *
+     * @param universe universe
+     */
+    public void setMyUniverse(Universe universe) {
+        getRepository().setMyUniverse(universe);
+    }
+}
