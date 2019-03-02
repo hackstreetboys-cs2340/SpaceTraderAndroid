ba175b05cfa72c494397ac344004dc2b2c1a1a41
Rahil Patel <rahilpatel99@gmail.com>
2019-03-01 23:13:47 -0500

    created Market model

---
+++ SpaceTraderIOS/SpaceTraderIOS/Model/Market.swift
@@ -0,0 +1,13 @@
+//
+//  Market.swift
+//  SpaceTraderIOS
+//
+//  Created by Rahil Patel on 3/1/19.
+//  Copyright © 2019 HackstreetBoys. All rights reserved.
+//
+
+import Foundation
+
+class Market {
+    
+}
