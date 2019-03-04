7775c0eb634feff852d01d12643fc2822780014a
Rahil Patel <rahilpatel99@gmail.com>
2019-03-04 00:20:40 -0500

    made good a variable

--- SpaceTraderIOS/SpaceTraderIOS/Other/CustomTableViewCells.swift
+++ SpaceTraderIOS/SpaceTraderIOS/Other/CustomTableViewCells.swift
@@ -15,7 +15,7 @@ class BuyItemCell: UITableViewCell {
     let goodQuantityLbl = UILabel()
     let quantityToBuyField = UITextField()
     let buyBtn = UIButton()
-    let good: MarketGood? = nil
+    var good: MarketGood? = nil
     override func layoutSubviews() {
         let offset: CGFloat = 20
         var width: CGFloat = 80
@@ -61,7 +61,7 @@ class SellItemCell: UITableViewCell {
     let goodPriceLbl = UILabel()
     let quantityToSellField = UITextField()
     let sellBtn = UIButton()
-    let good: MarketGood? = nil
+    var good: MarketGood? = nil
     override func layoutSubviews() {
         let offset: CGFloat = 20
         var width: CGFloat = 80
